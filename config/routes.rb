fc1ac5be9b93af7184f9db6b3c12ddf11242db7b
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-11-05 12:33:18 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'messages#index'
-  resources :messages, only: [:index]
-  resources :users, only: [:edit, :update]
-
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:nex, :create, :edet, :update] do
+  resources :messages, only: [:index, :create]
+  end
 end
