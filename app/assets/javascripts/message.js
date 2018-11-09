bba773ac501b56ca102c64594c1fe671a8019d0f
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-11-09 17:22:45 +0900

    HTMLを追加した分メッセージを下にスクロール

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,19 @@
 $(function(){
+
   function buildHTML(message){
     var html = `<div class="mainblock__chats__chat">
-                  <div class="mainblock__chats__chat__nickname">
-                    ${message.user_name}
-                  </div>
-                  <div class="mainblock__chats__chat__date">
-                    ${message.created_at}
-                  </div>`
-                    if(message.content !== "" || message.content === null){
-                      html = $(html).append(`<div class="mainblock__chats__chat__message">${message.content}</div>`)
-                    }
-                    if(message.image !== "" || message.image === null){
-                      html = $(html).append(`<div class="mainblock__chats__chat__image"><img src =${message.image}></div>`)
-                    }
-                `</div>`
+                  <div class="mainblock__chats__chat__nickname">${message.user_name}</div>
+                  <div class="mainblock__chats__chat__date">${message.created_at}</div>`
+                  if(message.content !== null){html = $(html).append(`<div class="mainblock__chats__chat__message">${message.content}</div>`)}
+                  if(message.image !== null){html = $(html).append(`<div class="mainblock__chats__chat__image"><img src =${message.image}></div>`)}
+               `</div>`
     return html;
+  }
+  function scroll() {
+    $('.mainblock__chats').animate({scrollTop: $('.height')[0].scrollHeight},0);
   }
   $('#js-form').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -27,17 +22,20 @@ $(function(){
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false,
-      disabled: false
+      contentType: false
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.mainblock__chats').append(html)
+      $('.height').append(html)
       $('.mainblock__form__message').val('')
       $('.hidden').val('')
+      $('.mainblock__form__submit').prop('disabled', false);
+      scroll()
     })
     .fail(function() {
-      alert('error');
-    });
+      alert('error')
+      $('.mainblock__form__submit').prop('disabled', false);
+    })
   });
+
 });
