2991ed66d1294522de7a5ea7483534982b50082a
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-11-09 13:21:13 +0900

    jsonからHTMLを作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,24 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="mainblock__chats__chat">
+                  <div class="mainblock__chats__chat__nickname">
+                    ${message.user_name}
+                  </div>
+                  <div class="mainblock__chats__chat__date">
+                    ${message.created_at}
+                  </div>`
+                    if(message.content !== "" || message.content === null){
+                      html = $(html).append(`<div class="mainblock__chats__chat__message">${message.content}</div>`)
+                    }
+                    if(message.image !== "" || message.image === null){
+                      html = $(html).append(`<div class="mainblock__chats__chat__image"><img src =${message.image}></div>`)
+                    }
+                `</div>`
+    return html;
+  }
   $('#js-form').on('submit', function(e){
     e.preventDefault();
+    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -9,6 +27,17 @@ $(function(){
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
-  })
-})
+      contentType: false,
+      disabled: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.mainblock__chats').append(html)
+      $('.mainblock__form__message').val('')
+      $('.hidden').val('')
+    })
+    .fail(function() {
+      alert('error');
+    });
+  });
+});
