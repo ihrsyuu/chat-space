083a026288b970b8f32fa749c469330d1514a820
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-11-10 20:00:01 +0900

    イベント発火、チャットメンバー追加

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -43,4 +43,21 @@ $(function() {
     }
   });
 
+  var search_list_add = $("#chat-group-users");
+
+  function appendUserNameAdd(user_name, user_id) {
+     var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+      search_list_add.append(html);
+  }
+
+  $("#user-search-result").on("click", "chat-group-user__btn--add", function() {
+    var user_name = $(this).data("user-name");
+    var user_id = $(this).data("user-id");
+    appendUserNameAdd(user_name, user_id);
+  });
+
 });
