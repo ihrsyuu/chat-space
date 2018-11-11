eaa8ff347723c4d0c4d11b40597777e83f5959c0
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-11-11 17:01:28 +0900

    自動更新　イベント発火作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,4 +40,23 @@ $(function(){
     })
   });
 
+  setInterval(function() {
+    $.ajax({
+      url: location.href.json,
+    })
+    .done(function(data) {
+    })
+    .fail(function(data) {
+    });
+    } else {
+    clearInterval(interval);
+    } , 5000 );
+  });
+
 });
+
+
+
+
+
+
