47044b9efb452cbaf9ca80e32c5504b86e8a573a
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-11-11 17:01:46 +0900

    コントローラ編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,13 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
+    @messages = @group.messages.order(created_at: :DESC).includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
     @users = @group.users
+
   end
 
   def create
